4c5732ae66eb276d6eb53ef2ebb40789d9a1c89a
Basilio <basilioherimiharisoa@gmail.com>
2025-07-31 15:24:58 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  $(".toggle-button").click(function () {
+    const icon = $(this).find("i");
+    icon.toggleClass("fa-bars fa-xmark");
+
+    $(".nav-links").toggleClass("show");
+  });
+
+  function initSlickIfNeeded() {
+    if ($(window).width() < 1024) {
+      if (!$(".slider-images").hasClass("slick-initialized")) {
+        $(".slider-images").slick({
+          infinite: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          dots: false,
+          autoplay: true,
+          speed: 800,
+          cssEase: "ease-in-out",
+          autoplaySpeed: 3000,
+        });
+      }
+    } else {
+      if ($(".slider-images").hasClass("slick-initialized")) {
+        $(".slider-images").slick("unslick");
+      }
+    }
+  }
+
+  initSlickIfNeeded();
+
+  $(window).on("resize", function () {
+    initSlickIfNeeded();
+  });
+});
